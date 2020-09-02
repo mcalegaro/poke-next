f72d8181add128fb84b62c86bd996727ff0b845a
Mário Calegaro <mcalegaro@gmail.com>
2020-09-02 01:29:26 -0300

    fix satic export paths

---
+++ next.config.js
@@ -0,0 +1,17 @@
+// module.exports = {
+//     exportPathMap: async function (
+//         defaultPathMap,
+//         { dev, dir, outDir, distDir, buildId }
+//     ) {
+//         return {
+//             '/': { page: '/' },
+//             '/detail': { page: '/detail/Detail' },
+//             '/list': { page: '/list' },
+//             '/search': { page: '/search/PokeSearch' },
+//         }
+//     },
+// }
+
+module.exports = {
+    exportTrailingSlash: true,
+}
