848d05ee1c3f4a08db8a39e45a95f175f0b23ea2
Mário Calegaro <mcalegaro@gmail.com>
2020-09-02 04:05:03 -0300

    logo

--- pages/_app.js
+++ pages/_app.js
@@ -9,9 +9,27 @@ function MyApp({ Component, pageProps }) {
         <title>PokéNext</title>
         <link rel="icon" href="/favicon.gif" type="image/gif" />
         <meta name="keywords" content="Pokémon, Pokemon, Pokémon Search, Pokemon Search, Pokémon Stats, Pokemon Stats" />
-        <meta name="description" content="Find your Pokémon" />
+        <meta name="description" content="Find your Pokémon!" />
         <meta name="author" content="Mário Calegaro" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        {/* <!-- Google / Search Engine Tags --> */}
+        <meta itemprop="name" content="PokéNext" />
+        <meta itemprop="description" content="Find your Pokémon!" />
+        <meta itemprop="image" content="http://pokenext.s3-website-sa-east-1.amazonaws.com/logo.png" />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="http://pokenext.s3-website-sa-east-1.amazonaws.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="PokéNext" />
+        <meta property="og:description" content="Find your Pokémon!" />
+        <meta property="og:image" content="http://pokenext.s3-website-sa-east-1.amazonaws.com/logo.png" />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="PokéNext" />
+        <meta name="twitter:description" content="Find your Pokémon!" />
+        <meta name="twitter:image" content="http://pokenext.s3-website-sa-east-1.amazonaws.com/logo.png" />
       </Head>
       <div className="container">
         <PokeMenu />
